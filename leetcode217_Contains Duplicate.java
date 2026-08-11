7e17746b42bc185183c9ac3467f2f182d0c4c898
VaishnaviBaje <vaishnavibaje215@gmail.com>
2026-08-11 21:37:33 +0530

    Implement containsDuplicate

---
+++ leetcode217_Contains Duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> myset=new HashSet<>();
+        for(int num:nums){
+            if(myset.contains(num)){
+                return true;
+            }
+            else{
+                myset.add(num);
+            }
+        }
+        return false;
+    }
+}
